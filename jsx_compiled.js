9de232f04cfbd2188661c9146f209e46e271e0b8
ygautomo <y_gautomo@yahoo.com>
2019-06-02 17:36:45 +0000

    Update jsx_compiled.js

---
+++ jsx_compiled.js
@@ -0,0 +1,17 @@
+class App extends React.Component {
+
+	render() {
+		return React.createElement(
+			"div",
+			{ className: "component-app" },
+			React.createElement(
+				"h1",
+				null,
+				"Test if this works"
+			)
+		);
+	}
+}
+
+export default App;
+
