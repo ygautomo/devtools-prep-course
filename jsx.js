c2e842d229e5336ce02bb928e2f7d4dfa8cf7fe2
ygautomo <y_gautomo@yahoo.com>
2019-06-03 00:20:51 +0700

    Add .babelrc

--- jsx.js
+++ jsx.js
@@ -1,11 +1,12 @@
 class App extends React.Component {
 
-  render() {
-    return (
-      <div className="component-app">
-        <h1>Test if this works</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="component-app">
+			<h1>Test if this works</h1>
+			</div>
+		);
+	}
 }
+
 export default App;
